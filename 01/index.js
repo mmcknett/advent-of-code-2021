e86ee960748cdcbb55a885187d8c7d4645afc48f
Matt McKnett <mmcknett@gmail.com>
2021-12-01 19:24:44 -0800

    Working part 2 solution

--- 01/index.js
+++ 01/index.js
@@ -6,6 +6,7 @@ var rl = readline.createInterface({
 });
 
 let increaseCount = 0;
+let sumIncreasedCount = 0;
 const numbers = [];
 
 rl.on('line', function(line) {
@@ -13,6 +14,7 @@ rl.on('line', function(line) {
   numbers.push(number);
 })
 
+// Handles part 1 of the prompt
 function countIncreases() {
   let previousValue = null;
 
@@ -28,11 +30,29 @@ function countIncreases() {
   }
 }
 
+// Handles part 2 of the prompt
 function countTriplesIncreases() {
-  
+  let previousSum = null;
+
+  for (let i = 0; i < numbers.length - 2; ++i) {
+    let incDecString = "(N/A - no previous sum)";
+    const tripleSum = numbers[i] + numbers[i+1] + numbers[i+2];
+
+    if (previousSum !== null) {
+      let diff = tripleSum - previousSum;
+      sumIncreasedCount += diff > 0 ? 1 : 0;
+
+      incDecString = diff > 0 ? "(increased)" : diff < 0 ? "(decreased)" : "(no change)";
+    }
+
+    previousSum = tripleSum;
+    console.log(tripleSum, incDecString);
+  }
 }
 
 rl.on('close', function() {
   countIncreases();
+  countTriplesIncreases();
   console.log("Number of increases:", increaseCount);
+  console.log("Number of triple-sum increases:", sumIncreasedCount);
 })
