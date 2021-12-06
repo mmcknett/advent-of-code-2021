4c19f752721f7f7b429c651a8c2c0bedfc36700c
Matt McKnett <mmcknett@gmail.com>
2021-12-05 23:07:24 -0800

    refactor a bit

--- 06/main.cpp
+++ 06/main.cpp
@@ -6,6 +6,34 @@
 #include <numeric>
 using namespace std;
 
+void readCommaSeparatedValues(uint64_t fish[]);
+uint64_t countFish(uint64_t fish[]);
+string counts_str(uint64_t fish[]);
+
+const uint64_t SIZE = 9;
+
+int main() {
+  uint64_t fish[SIZE] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+  const int simulateDays = 256;
+
+  readCommaSeparatedValues(fish);
+
+  for (int i = 0; i < simulateDays; ++i) {
+    uint64_t newFish = fish[0];
+    for (int f = 1; f < SIZE; ++f) {
+      fish[f-1] = fish[f];
+    }
+    fish[8] = newFish;
+    fish[6] += newFish;
+
+    cout << "After " << i + 1 << " days: " << counts_str(fish) << " - " << countFish(fish) << " fish" << endl;
+  }
+
+  cout << "After " << simulateDays << " days there are " << countFish(fish) << " fish." << endl;
+
+  return 0;
+}
+
 void readCommaSeparatedValues(uint64_t fish[]) {
   while(true) {
     int i;
@@ -19,36 +47,17 @@ void readCommaSeparatedValues(uint64_t fish[]) {
   cout << endl;
 }
 
-int main() {
-  uint64_t fish[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
-  const int simulateDays = 256;
-
-  readCommaSeparatedValues(fish);
-
-  for (int i = 0; i < simulateDays; ++i) {
-    uint64_t newFish = fish[0];
-    for (int f = 1; f < 9; ++f) {
-      fish[f-1] = fish[f];
-    }
-    fish[8] = newFish;
-    fish[6] += newFish;
-
-    uint64_t fishcount = 0;
-    for (int i = 0; i < 9; ++i) {
-      fishcount += fish[i];
-    }
-
-    ostringstream oss;
-    copy(fish, fish + 8, ostream_iterator<uint64_t>(oss, ","));
-    oss << fish[8];
-    cout << "After\t" << i + 1 << " days:\t" << oss.str() << " " << fishcount << " fish" << endl;
-  }
-
+uint64_t countFish(uint64_t fish[]) {
   uint64_t fishcount = 0;
-  for (int i = 0; i < 9; ++i) {
+  for (int i = 0; i < SIZE; ++i) {
     fishcount += fish[i];
   }
-  cout << "After " << simulateDays << " days there are " << fishcount << " fish." << endl;
+  return fishcount;
+}
 
-  return 0;
+string counts_str(uint64_t fish[]) {
+  ostringstream oss;
+  copy(fish, fish + SIZE - 1, ostream_iterator<uint64_t>(oss, ","));
+  oss << fish[SIZE - 1];
+  return oss.str();
 }
