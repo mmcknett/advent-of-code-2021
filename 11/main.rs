dba36e135cdb470f8f483d3b6136a3fd407503d4
Matt McKnett <mmcknett@gmail.com>
2021-12-10 22:34:57 -0800

    Refactor
    - Aliased the type for the grid of octopi
    - Removed the extra vectors holding flashed squares
    - Consolidated incrementing squares in the propagate_flashes function
    - Limited flash reporting to the first 100 steps (i.e. restored part 1
      functionality)

--- 11/main.rs
+++ 11/main.rs
@@ -1,6 +1,8 @@
 use std::io::{self, BufRead};
 use std::cmp;
 
+type Octogrid = [[i8; 10]; 10];
+
 fn main() {
     let mut octos = [[0; 10]; 10];
     load_octos(&mut octos);
@@ -9,24 +11,17 @@ fn main() {
 
     // Part 1
     for step in 1..=10000 {
-        let mut initial_flashes = vec![];
-        // First, increase every energy level
+        // First, increment energy and propagate flashes.
         for y in 0..10 {
             for x in 0..10 {
-                octos[x][y] += 1;
-                if octos[x][y] > 9 {
-                    initial_flashes.push((x, y));
-                    flashes += 1;
-                }
+                let new_flashes = propagate_flashes((x, y), &mut octos);
+
+                // Only count flashes up to step 100 for part 1.
+                if step <= 100 { flashes += new_flashes; }
             }
         }
 
-        // Count and propagate flashes (anything that becomes 10 is a flash)
-        for octo in initial_flashes {
-            flashes += propagate_flashes(octo, &mut octos);
-        }
-
-        // Last, clamp to 0.
+        // Go back through and clamp to 0.
         for y in 0..10 {
             for x in 0..10 {
                 if octos[x][y] > 9 {
@@ -42,14 +37,15 @@ fn main() {
             next_row.iter().all(|&octo| octo == 0)
         );
         if all_zero {
+            // For part 2, stop when we find a full flash.
             break;
         }
     }
 
-    print!("\nTotal flahes: {}\n", flashes);
+    print!("\nTotal flahes at step 100: {}\n", flashes);
 }
 
-fn load_octos(octos: &mut [[i8; 10]; 10]) {
+fn load_octos(octos: &mut Octogrid) {
     for (y, line) in io::stdin().lock().lines().enumerate() {
         for (x, num_char) in line.unwrap().chars().into_iter().enumerate() {
             octos[x][y] = num_char.to_digit(10).unwrap() as i8;
@@ -60,7 +56,7 @@ fn load_octos(octos: &mut [[i8; 10]; 10]) {
     print_octos(octos);
 }
 
-fn print_octos(octos: &[[i8; 10]; 10]) {
+fn print_octos(octos: &Octogrid) {
     for y in 0..10 {
         for x in 0..10 {
             print!("{}", octos[x][y]);
@@ -69,27 +65,25 @@ fn print_octos(octos: &[[i8; 10]; 10]) {
     }
 }
 
-fn propagate_flashes((x, y): (usize, usize), mut octos: &mut [[i8; 10]; 10]) -> u64 {
+fn propagate_flashes((x, y): (usize, usize), mut octos: &mut Octogrid) -> u64 {
     let mut flash_count = 0;
-    let mut next_flashes = vec![];
 
-    let min_x = if x > 0 { x - 1 } else { 0 };
-    let min_y = if y > 0 { y - 1 } else { 0 };
-    let max_x = cmp::min(9, x + 1);
-    let max_y = cmp::min(9, y + 1);
+    octos[x][y] += 1;
+    if octos[x][y] == 10 {
+        flash_count += 1;
 
-    for y_next in min_y..=max_y {
-        for x_next in min_x..=max_x {
-            octos[x_next][y_next] += 1;
-            if octos[x_next][y_next] == 10 {
-                next_flashes.push((x_next, y_next));
-                flash_count += 1;
+        let min_x = if x > 0 { x - 1 } else { 0 };
+        let min_y = if y > 0 { y - 1 } else { 0 };
+        let max_x = cmp::min(9, x + 1);
+        let max_y = cmp::min(9, y + 1);
+
+        for y_next in min_y..=max_y {
+            for x_next in min_x..=max_x {
+                if x != x_next || y != y_next {
+                    flash_count += propagate_flashes((x_next, y_next), &mut octos);
+                }
             }
         }
-    }
-
-    for octo in next_flashes {
-        flash_count += propagate_flashes(octo, &mut octos);
     }
 
     return flash_count;
