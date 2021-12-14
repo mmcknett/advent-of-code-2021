9e4b4c13e33c814d21c6a93220c6df69b484422c
Matt McKnett <mmcknett@gmail.com>
2021-12-13 23:43:21 -0800

    Refactoring to try to address part 2. This works with basic.txt

--- 14/src/main.rs
+++ 14/src/main.rs
@@ -1,7 +1,7 @@
 use std::io::{self, BufRead};
 use std::collections::HashMap;
 
-type InsertionRules = HashMap<String, String>;
+type InsertionRules = HashMap<(char, char), char>;
 
 fn main() {
   let mut prior_step = String::new();
@@ -10,6 +10,10 @@ fn main() {
   println!("Template:\t{}", prior_step);
 
   let mut rules: InsertionRules = HashMap::new();
+
+  // let mut bigram_list = vec![];
+  // let mut bigram_to_index: HashMap<&str, u8> = HashMap::new();
+  // let mut bigram_rules: HashMap<&str, u8> = HashMap::new();
 
   for line in io::stdin().lock().lines() {
     let linestr = line.unwrap();
@@ -20,31 +24,40 @@ fn main() {
 
     let mut frto = linestr.split(" -> ");
     let fr = frto.next().unwrap();
-    let to = frto.next().unwrap();
+    let to: char = frto.next().unwrap().chars().next().unwrap();
 
-    if rules.contains_key(fr) {
+    let mut cs = fr.chars();
+    let char_pair: (char, char) = (cs.next().unwrap(), cs.next().unwrap());
+
+    if rules.contains_key(&char_pair) {
       panic!("Didn't expect duplicate keys");
     }
 
-    rules.insert(String::from(fr), String::from(to));
+    rules.insert(char_pair, to);
   }
 
-  for step in 1..=10 {
-    let mut step_result = String::from(&prior_step[0..1]);
-    for i in 0..(prior_step.len() - 1) {
-      let twochars = String::from(&prior_step[i..=i+1]);
-      let insert_char = rules.get(&twochars).unwrap();
-      step_result.push_str(insert_char);
-      step_result.push_str(&twochars[1..=1]);
-    }
+  // for step in 1..=10 {
+  //   let mut step_result = String::from(&prior_step[0..1]);
+  //   for i in 0..(prior_step.len() - 1) {
+  //     let twochars = String::from(&prior_step[i..=i+1]);
+  //     let insert_char = rules.get(&twochars).unwrap();
+  //     step_result.push_str(insert_char);
+  //     step_result.push_str(&twochars[1..=1]);
+  //   }
 
-    println!("{}\nAfter step {}: ({})", step_result, step, step_result.len());
-    prior_step = step_result;
-  }
+  //   println!("{}\nAfter step {}: ({})", step_result, step, step_result.len());
+  //   prior_step = step_result;
+  // }
 
-  let char_counts = prior_step.chars().fold(HashMap::new(), |mut hash, c| {
-    *hash.entry(c).or_insert(0u32) += 1; hash
-  });
+  // let char_counts = prior_step.chars().fold(HashMap::new(), |mut hash, c| {
+  //   *hash.entry(c).or_insert(0u32) += 1; hash
+  // });
+
+  let start = &prior_step;
+
+  counts_from_bigram_start(start, 1, &rules);
+  counts_from_bigram_start(start, 2, &rules);
+  let char_counts = counts_from_bigram_start(start, 3, &rules);
 
   let max_char: (&char, &u32) = char_counts
     .iter()
@@ -62,6 +75,53 @@ fn main() {
   println!("Min: ({}, {})", min_char.0, min_char.1);
   let diff = max_char.1 - min_char.1;
   println!("Desired difference: {}", diff);
+}
+
+fn counts_from_bigram_start(
+  start: &str,
+  depth: u8,
+  rules: &InsertionRules) -> HashMap<char, u32>
+{
+  let first_char = start.chars().next().unwrap();
+  let mut char_counts = HashMap::from([
+    (first_char, 1)
+  ]);
+
+  let mut left = start.chars();
+  let mut right = start.chars();
+  right.next();
+
+  let mut strs_result = vec![];
+
+  while let Some(c_r) = right.next() {
+    *char_counts.entry(c_r).or_insert(0u32) += 1;
+    let c_l = left.next().unwrap();
+    let res = counts_from_bigram((c_l, c_r), depth, &rules, &mut char_counts);
+    strs_result.push(res);
+  }
+
+  println!("{}{}", first_char, strs_result.join(""));
+
+  return char_counts;
+}
+
+fn counts_from_bigram(
+  bigram: (char, char),
+  depth: u8,
+  rules: &InsertionRules,
+  char_counts: &mut HashMap<char, u32>) -> String
+{
+  if depth == 0 {
+    return String::from(bigram.1);
+  }
+
+  let c = rules.get(&bigram).unwrap();
+  *char_counts.entry(*c).or_insert(0u32) += 1;
+
+  let left = counts_from_bigram((bigram.0, *c), depth - 1, &rules, char_counts);
+  let right = counts_from_bigram((*c, bigram.1), depth - 1, &rules, char_counts);
+
+  return left + &right;
 }
 
 
