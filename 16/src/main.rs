1f074a38ea70e01bd2e02c636487dca6120c7d81
Matt McKnett <mmcknett@gmail.com>
2021-12-17 00:41:07 -0800

    Day 16 Part 1 working

--- 16/src/main.rs
+++ 16/src/main.rs
@@ -1,7 +1,12 @@
+#[macro_use]
+extern crate enum_primitive_derive;
+extern crate num_traits;
+
 use std::io::{self, Read};
+use num_traits::{FromPrimitive, ToPrimitive};
 
 fn main() {
-    let mut buffer: Vec<u8> = Vec::new();
+    let mut buffer: Bitstream = Vec::new();
     io::stdin().lock().read_to_end(&mut buffer);
 
     buffer = buffer.iter().filter(|&&c| c != 10 && c != 13).map(|c| match *c as char {
@@ -10,25 +15,121 @@ fn main() {
         _ => panic!("Not a hex digit")
     }).collect();
 
-    println!("{:?}", buffer);
+    let mut packet_parser = PacketParser {
+        biter: Biter::new(&buffer),
+        bits_consumed: 0,
+        total_versions: 0
+    };
+    packet_parser.read_packet();
+
+    println!("Version sum is: {}", packet_parser.total_versions);
 }
 
-const VERSION_MASK: u64 = 0xE00000;
-
-fn extract_version(packet: &Vec<u8>) -> u8 {
-    let version = (packet[0] & 0xE) >> 1;
-    return version;
+struct PacketParser<'a> {
+    biter: Biter<'a>,
+    bits_consumed: u32,
+    total_versions: u32
 }
 
-struct Bitstream {
-    nibble_vec: Vec<u8>,
+impl<'a> PacketParser<'a> {
+    fn next(&mut self, bit_count: u8) -> u16 {
+        self.bits_consumed += bit_count as u32;
+        self.biter.next(bit_count)
+    }
+
+    fn read_packet(&mut self) {
+        let header = self.read_header();
+        println!("{:?}", header);
+
+        
+        if header.packet_type_id == Operator::Literal {
+            self.read_literal_body();
+        } else {
+            self.read_operator_body();
+        }
+    }
+
+    fn read_header(&mut self) -> Header {
+        let version = self.next(3) as u8;
+        let packet_type_id = Operator::from_u16(self.next(3)).unwrap();
+
+        self.total_versions += version as u32;
+
+        return Header {
+            version,
+            packet_type_id
+        };
+    }
+
+    fn read_operator_body(&mut self) {
+        let length_type = self.next(1);
+        if length_type == 0 {
+            let subpacket_size = self.next(15) as u32;
+
+            let start = self.bits_consumed;
+            while subpacket_size > (self.bits_consumed - start) {
+                self.read_packet();
+            }
+        } else {
+            let subpacket_count = self.next(11);
+
+            for _ in 0..subpacket_count {
+                self.read_packet();
+            }
+        }
+    }
+
+    fn read_literal_body(&mut self) {
+        // Literals are 5-bit groups; a 1 indicates there
+        // are more nibbles; a 0 indicates this is the last nibble.
+        loop {
+            let more = self.next(1) != 0;
+            let _ = self.next(4); // Read a nibble
+            if !more { break; }
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Header {
+    version: u8,
+    packet_type_id: Operator
+}
+
+#[repr(u8)]
+#[derive(Primitive, Debug, Eq, PartialEq, Clone, Copy)]
+enum Operator  {
+    Sum = 0,
+    Product = 1,
+    Minimum = 2,
+    Maximum = 3,
+    Literal = 4,
+    GreaterThan = 5,
+    LessThan = 6,
+    Equals = 7
+}
+
+type Bitstream = Vec<u8>;
+
+trait Biterator {
+    fn biter(&self) -> Biter;
+}
+
+struct Biter<'a> {
+    nibble_vec: &'a Bitstream,
     vec_idx: usize,
     bit_idx: u8
 }
 
-impl Bitstream {
-    fn new(nibble_vec: Vec<u8>) -> Bitstream {
-        Bitstream {
+impl Biterator for Bitstream {
+    fn biter(&self) -> Biter {
+        Biter::new(&self)
+    }
+}
+
+impl<'a> Biter<'a> {
+    fn new(nibble_vec: &Bitstream) -> Biter {
+        Biter {
             nibble_vec: nibble_vec,
             vec_idx: 0,
             bit_idx: 0,
@@ -43,39 +144,43 @@ impl Bitstream {
         let mut bit_count = bit_count;
         let mut result = 0u16;
 
-        // Cases to deal with...
-        // 1 bit requested, bit_idx is 3 -> 2, 2 -> 1 or 1 -> 0
-        // 2 bits requested, bit_idx is 3 -> 1 or 2 -> 0
-        // 3 bits requested, bit_idx is 3 -> 0
+        if self.bit_idx != 0 {
+            // Eat bits one at a time til the next nibble
+            while self.bit_idx > 0 && bit_count > 0 {
+                let offset = self.bit_idx - 1;
+                let nextbit = ((0x1 << offset) & self.nibble_vec[self.vec_idx]) >> offset;
+                result = (result << 1) + nextbit as u16;
 
-        // Consume the bits remaining in the current nibble first.
-        let bitmask = match bit_count {
-            1 => 0x1,
-            2 => 0x3,
-            3 => 0x7,
-            0 => 0xF,
-            _ => panic!("WAT")
-        };
+                self.bit_idx -= 1;
+                bit_count -= 1;
+            }
 
-
-        // Consume blocks of 4 bits, aligned on nibble boundaries.
-        while bit_count >= 4 {
-            let nibble = self.nibble_vec[self.vec_idx];
-            result = (result << 4) + nibble as u16;
-
-            self.vec_idx += 1;
-            bit_count -= 4;
+            // We stopped because we consumed the last bit
+            if self.bit_idx == 0 {
+                self.vec_idx += 1;
+            }
         }
 
-        // Consume the remaining bits
-        let nibble = self.nibble_vec[self.vec_idx];
-        result += match bit_count {
-            0 => { self.bit_idx = 0; 0 },
-            1 => { self.bit_idx = 3; (nibble as u16 & 0x8) >> 3 },
-            2 => { self.bit_idx = 2; (nibble as u16 & 0xC) >> 2 },
-            3 => { self.bit_idx = 1; (nibble as u16 & 0xE) >> 1 },
-            _ => panic!("This bitcount shouldn't be possible!")
-        };
+        if bit_count > 0 {
+            // Consume blocks of 4 bits, aligned on nibble boundaries.
+            while bit_count >= 4 {
+                let nibble = self.nibble_vec[self.vec_idx];
+                result = (result << 4) + nibble as u16;
+
+                self.vec_idx += 1;
+                bit_count -= 4;
+            }
+
+            // Consume the remaining bits
+            let nibble = self.nibble_vec[self.vec_idx];
+            result = (result << bit_count) +  match bit_count {
+                0 => { self.bit_idx = 0; 0 },
+                1 => { self.bit_idx = 3; (nibble as u16 & 0x8) >> 3 },
+                2 => { self.bit_idx = 2; (nibble as u16 & 0xC) >> 2 },
+                3 => { self.bit_idx = 1; (nibble as u16 & 0xE) >> 1 },
+                _ => panic!("This bitcount shouldn't be possible!")
+            };
+        }
 
         return result;
     }
@@ -83,7 +188,7 @@ impl Bitstream {
 
 #[cfg(test)] 
 mod tests {
-    use super::{extract_version, Bitstream};
+    use super::{Bitstream, Biterator};
 
     const LITERAL_PACKET: [u8; 6] = [0b1101, 0b0010, 0b1111, 0b1110, 0b0010, 0b1000];
     const OPERATOR_PACKET: [u8; 14] = [0x3, 0x8, 0x0, 0x0, 0x6, 0xF, 0x4, 0x5, 0x2, 0x9, 0x1, 0x2, 0x0, 0x0];
@@ -91,29 +196,24 @@ mod tests {
 
     #[test]
     fn bitstream_gets_correct_bits() {
-        let mut bitstream = Bitstream::new(Vec::from(LITERAL_PACKET));
-        assert_eq!(6, bitstream.next(3));
-        assert_eq!(4, bitstream.next(3));
-        assert_eq!(0b10111, bitstream.next(5));
-        assert_eq!(0b11110, bitstream.next(5));
-        assert_eq!(0b00101, bitstream.next(5));
+        let bitstream: Bitstream = Bitstream::from(LITERAL_PACKET);
+        let mut biter = bitstream.biter();
+        assert_eq!(6, biter.next(3));
+        assert_eq!(4, biter.next(3));
+        assert_eq!(0b10111, biter.next(5));
+        assert_eq!(0b11110, biter.next(5));
+        assert_eq!(0b00101, biter.next(5));
     }
 
     #[test]
-    fn it_extracts_the_version() {
-        let actual = extract_version(&Vec::from(LITERAL_PACKET));
-        assert_eq!(6, actual);
-    }
-
-    #[test]
-    fn it_extracts_the_version_for_operator_packet() {
-        let actual = extract_version(&Vec::from(OPERATOR_PACKET));
-        assert_eq!(1, actual);
-    }
-
-    #[test]
-    fn it_extracts_the_version_for_operator_packet_v4() {
-        let actual = extract_version(&Vec::from(OPERATOR_PACKET_V4));
-        assert_eq!(4, actual);
+    fn bitstream_gets_correct_bits_operator() {
+        let bitstream: Bitstream = Bitstream::from(OPERATOR_PACKET);
+        let mut biter = bitstream.biter();
+        assert_eq!(1, biter.next(3));
+        assert_eq!(6, biter.next(3));
+        assert_eq!(0, biter.next(1));
+        assert_eq!(27, biter.next(15));
+        assert_eq!(0x68a, biter.next(11));
+        assert_eq!(0x5224, biter.next(16));
     }
 }
