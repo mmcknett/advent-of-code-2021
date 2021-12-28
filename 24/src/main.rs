3b217d0ad7dadac1c2d06ef7e383451e09640032
Matt McKnett <mmcknett@gmail.com>
2021-12-28 00:56:03 -0800

    It's a mess, but this got me Day24 part 1 & 2.

--- 24/src/main.rs
+++ 24/src/main.rs
@@ -41,17 +41,33 @@ fn main() {
     // println!("For the example {}, z is {}", example, aluvm.vars.get("z").unwrap());
     // aluvm.reset();
 
-    let hi_so_far = 13997997296975i64;
+    // let hi_so_far = 13997997296975i64;
+    // 13997991296953 is too low!
+
+    // Part 1...
+    // See transcriptions.txt -- This number was arrived at by looking at how
+    // the x value, as observed during the x == w check, changed across each run.
+    // Any time changing a digit made it so an x value came within 1..=9, the digit
+    // was modified to make it so that the x value was its highest possible.
+    // let hi_so_far = 939_9_7_99_929_69_1_2i64;
+
+    // Part 2...
+    // Doing some exploration turns up that you have to have at least 8 as
+    // the first digit.
+    // I came up with the necessary prefix 8111137, and brute-forced the remaining
+    // digits from that point.
+    // let hi_so_far = 811_1_1_37_194_11_1_1i64;
+    let hi_so_far = 811_1_1_37_914_18_1_1i64;
+
     aluvm.run_on_input(&get_as_vec(hi_so_far));
     println!("");
     aluvm.printvars();
-    println!("For the hi so far {}, z is {}", hi_so_far, aluvm.vars.get("z").unwrap());
+    println!("For the hi so far {}, z is {}, as base26: {}", hi_so_far, aluvm.vars.get("z").unwrap(), aluvm.get_z_base26());
     aluvm.reset();
 
-    // 13997991296953 is too low!
+    // part_1_by_parts(aluvm);
 
-    //part_1_by_parts(aluvm);
-
+    // Used this to brute-force part 2.
     // run_part1(aluvm);
 }
 
@@ -61,15 +77,15 @@ fn part_1_by_parts(mut aluvm: Aluvm) {
     const TAKE: usize = 100000;
 
     for i in 1..=9 {
-        for i2 in 1..=9 {
-            for i3 in 1..=9 {
-                for i4 in 1..=9 {
-                    for i5 in 1..=9 {
-                        for i6 in 1..=9 {
+        // for i2 in 1..=9 {
+        //     for i3 in 1..=9 {
+        //         for i4 in 1..=9 {
+        //             for i5 in 1..=9 {
+        //                 for i6 in 1..=9 {
                             // for i7 in 1..=9 {
                                 // for idx8 in 1..=9 {
                                     //for i9 in 1..=9 {
-                                        let input = vec![i, i2, i3, i4, i5, i6]; //, i7]; //, idx8]; //, i9];
+                                        let input = vec![i]; //, i2, i3, i4, i5, i6]; //, i7]; //, idx8]; //, i9];
                                         aluvm.reset();
                                         match aluvm.run_on_input(&input) {
                                             Ok(_) => (),
@@ -82,11 +98,11 @@ fn part_1_by_parts(mut aluvm: Aluvm) {
                                     //}
                                 // }
                             // }
-                        }
-                    }
-                }
-            }
-        }
+        //                 }
+        //             }
+        //         }
+        //     }
+        // }
     }
 
     let sortfn = |(a_z, a_vec): &(i64, Vec<i64>), (b_z, b_vec): &(i64, Vec<i64>)| {
@@ -94,11 +110,11 @@ fn part_1_by_parts(mut aluvm: Aluvm) {
         return zcmp;
     };
 
-    results.sort_by(&sortfn);
-    results = results.iter().take(TAKE).cloned().collect();
-    for (i, result) in results.iter().enumerate() {
-        println!("{}: {:?}", i, result);
-    }
+    // results.sort_by(&sortfn);
+    // results = results.iter().take(TAKE).cloned().collect();
+    // for (i, result) in results.iter().enumerate() {
+    //     println!("{}: {:?}", i, result);
+    // }
 
     // THIS ONLY DOES 13 DIGITS :facepalm:
     while results[0].1.len() < 14 {
@@ -107,7 +123,16 @@ fn part_1_by_parts(mut aluvm: Aluvm) {
         let mut next_digit_results = vec![];
 
         for (_, res_vec) in &results {
-            for i in 1..=9 {
+            let x = *aluvm.vars.get("x").unwrap();
+            let rg = match x {
+                1..=9 => {
+                    println!("x is {}", x);
+                    x..=x
+                },
+                _ => 1..=9
+            };
+            
+            for i in rg {
                 // for i2 in 1..=9 {
                     // for i3 in 1..=9 {
                     //     for i4 in 1..=9 {
@@ -149,14 +174,21 @@ fn run_part1(mut aluvm: Aluvm) {
     // let mut curr = 99999987172995i64; // Already check everything higher than this.
 
     // let mut curr = 11111111111111i64;
+    
 
     // let mut curr = 99999893100000i64; // I *think* I correctly checked values higher than this, but not sure.
     // let mut curr = 99999891951648i64;
-    let mut curr = 99999877539328i64;
+    // let mut curr = 99999877539328i64;
+
+    let mut curr = 811_1_1_37_1111111i64;
+    let mut lowest_z = i64::MAX;
+
     loop {
         // curr -= 13;
-        curr -= 1;
-        if curr < 11111111111111i64 {
+        curr += 1;
+
+        // I just want to search things starting with 811_1_1_37_
+        if curr > 811_1_1_37_9999999i64 {
             println!("Not found.");
             break; // None found.
         }
@@ -183,8 +215,9 @@ fn run_part1(mut aluvm: Aluvm) {
             break;
         }
 
-        if curr % 17576 == 0 {
-            println!("{} results in {:#x}", curr, z);
+        if z < lowest_z {
+            println!("Lowest so far is {} for {}", aluvm.get_z_base26(), curr);
+            lowest_z = z;
         }
 
         // if (curr % 2451) == 0 {
@@ -192,7 +225,7 @@ fn run_part1(mut aluvm: Aluvm) {
         // }
     }
 
-    println!("Largest MONAD-accepted model number: {}", curr);
+    println!("Smallest MONAD-accepted model number: {}", curr);
 }
 
 // fn gen_vec(a: i64) -> Vec<i64> {
@@ -258,7 +291,7 @@ impl Aluvm {
     fn run_on_input(&mut self, vals: &Vec<i64>) -> Result<(), ()> {
         let mut valiter = vals.iter();
 
-        for i in &mut self.inst {
+        for i in &self.inst {
             if Self::run_inst(&mut self.vars, i.clone(), &mut valiter).is_err() {
                 return Err(());
             }
@@ -267,13 +300,26 @@ impl Aluvm {
         return Ok(());
     }
 
+    fn brk_on_eq(&mut self, instructions: &mut std::slice::Iter<Inst>, valiter: &mut std::slice::Iter<i64>) -> Result<(), ()> {
+        for i in instructions {
+            if let Inst::Eql(_, _) = i {
+                break;
+            }
+
+            if Self::run_inst(&mut self.vars, i.clone(), valiter).is_err() {
+                return Err(());
+            }
+        }
+        return Ok(());
+    }
+
     fn run_inst(vars: &mut Variables, i: Inst, valiter: &mut std::slice::Iter<i64>) -> Result<(), ()> {
         match i {
             Inst::Inp(vname) => {
                 match valiter.next() {
                     Some(val) => {
-                        println!("State on input {}:", val);
-                        Self::print(&vars);
+                        // println!("State on input {}:", val);
+                        // Self::print(&vars);
                         vars.insert(vname, *val)
                     },
                     None => return Err(())
@@ -300,9 +346,14 @@ impl Aluvm {
                 vars.insert(vname, a % b);
             },
             Inst::Eql(vname, val) => {
-                println!("x is {} on an eql operation", vars.get("x").unwrap());
                 let a = *vars.get(&vname).unwrap();
                 let b = Self::get_val(val, vars);
+
+                // Debugging if b is not 0
+                if b != 0 {
+                    println!("x is {}, other is {}", vars.get("x").unwrap(), b);
+                }
+
                 vars.insert(vname, if a == b { 1 } else { 0 });
             }
         }
@@ -341,6 +392,20 @@ impl Aluvm {
             vars.get("y").unwrap(),
             vars.get("z").unwrap(),
         );
+    }
+
+    fn get_z_base26(&self) -> String {
+        let mut z = *self.vars.get("z").unwrap();
+        let mut res = vec![];
+
+        while z > 0 {
+            let c = ((z % 26) as u8 + 'A' as u8) as char;
+            res.push(c);
+            z /= 26;
+        }
+
+        res.reverse();
+        return res.iter().collect::<String>();
     }
 }
 
