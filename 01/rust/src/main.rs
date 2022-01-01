a9c755025152ea2b024179a4c884faa622f699c7
Matt McKnett <mmcknett@gmail.com>
2021-12-31 23:48:23 -0800

    refactor to use iterator functions

--- 01/rust/src/main.rs
+++ 01/rust/src/main.rs
@@ -2,24 +2,35 @@ use std::io::{self, BufRead};
 
 fn main() {
   let depths = load_depths();
-  let mut count_increases = 0;
-  for i in 1..depths.len() {
-    count_increases += if depths[i] > depths[i-1] { 1 } else { 0 };
-  }
-  println!("there are {} increases", count_increases);
+  let count = count_increases(&depths);
+  println!("there are {} increases", count);
 
-  let mut sums = vec![];
-  for i in 2..depths.len() {
-    sums.push(depths[i] + depths[i-1] + depths[i-2]);
-  }
-
-  count_increases = 0;
-  for i in 1..sums.len() {
-    count_increases += if sums[i] > sums[i-1] { 1 } else { 0 };
-  }
-  println!("there are {} triple-sum increases", count_increases);
+  let sums = triple_sums(&depths);
+  let count = count_increases(&sums);
+  println!("there are {} triple-sum increases", count);
 }
 
 fn load_depths() -> Vec<i32> {
-  io::stdin().lock().lines().map(|s| s.unwrap().parse::<i32>().unwrap()).collect::<Vec<i32>>()
+  io::stdin()
+    .lock()
+    .lines()
+    .map(
+      |s| s.unwrap().parse::<i32>().unwrap()
+    ).collect::<Vec<i32>>()
+}
+
+fn count_increases(list: &Vec<i32>) -> u32 {
+  list
+    .windows(2)
+    .fold(
+      0u32,
+      |accum, slice| accum + if slice[1] > slice[0] { 1 } else { 0 }
+    )
+}
+
+fn triple_sums(list: &Vec<i32>) -> Vec<i32> {
+  list
+    .windows(3)
+    .map(|slice| slice.iter().sum())
+    .collect()
 }
